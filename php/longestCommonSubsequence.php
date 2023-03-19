08d66806019bc867d996aa9a492b933776c460be
James Pow <jbpow18@icloud.com>
2023-03-19 15:47:13 -0400

    feat: first commit

---
+++ php/longestCommonSubsequence.php
@@ -0,0 +1,26 @@
+<?php
+
+  function longestCommonSubsequence(string $str1, string $str2): string {
+    if (strlen($str1) < 1 || strlen($str2) < 1) {
+      return "One of the strings is empty.";
+    }
+
+    $subsequence = "";
+
+    foreach (str_split($str1) as $char1) {
+      foreach (str_split($str2) as $char2) {
+        if ($char1 === $char2) {
+          $subsequence .= $char1;
+        }
+      }
+    }
+
+    return $subsequence;
+  }
+
+  $string1 = "ABCDGH";
+  $string2 = "AEDFHR";
+
+  echo longestCommonSubsequence($string1, $string2);
+
+?>
