e59eaa3086627d5d2a6a05a52883afb453d8020a
James Pow <jbpow18@icloud.com>
2023-03-19 16:35:11 -0400

    feat: added documentation

--- php/longestCommonSubsequence.php
+++ php/longestCommonSubsequence.php
@@ -1,8 +1,16 @@
 <?php
 
-  function longestCommonSubsequence(string $str1, string $str2): string {
+  /*
+  * Searches two strings and returns the longest common subsequence
+  * If either of the input strings are empty, the function returns null.
+  *
+  * @param two strings to compare
+  * @return string of longest common subsequence or null if either string
+  * is empty.
+  */
+  function longestCommonSubsequence(string $str1, string $str2): ?string {
     if (strlen($str1) < 1 || strlen($str2) < 1) {
-      return "One of the strings is empty.";
+      return null;
     }
 
     $subsequence = "";
@@ -21,6 +29,12 @@
   $string1 = "ABCDGH";
   $string2 = "AEDFHR";
 
-  echo longestCommonSubsequence($string1, $string2);
+  $result = longestCommonSubsequence($string1, $string2);
+
+  if ($result === null) {
+    echo "One of the strings is empty.";
+  } else {
+    echo $result;
+  }
 
 ?>
