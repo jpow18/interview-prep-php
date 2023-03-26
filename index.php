e6ab1e237b58e6e3c61e6230b18eb69351c3e5a1
James Pow <jbpow18@icloud.com>
2023-03-25 21:28:04 -0400

    feat: added new challenges

--- index.php
+++ index.php
@@ -3,6 +3,16 @@
 
   //include_once "./php/sumFirstLastArray.php";
 
-  include_once "./php/secondLargestIntArray.php";
+  //include_once "./php/secondLargestIntArray.php";
+
+  //include_once "./php/longestCommonSubsequence.php";
+
+  //include_once "./php/kadanesAlgorithm.php";
+
+  //include_once "./php/findSumPairs.php";
+
+  //include_once "./php/longestPalindrome.php";
+
+  include_once "./php/numWords.php";
   
 ?>
