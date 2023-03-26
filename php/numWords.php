5e83e3d3d58f0b9c4ba0f3f664fc63c6fad0972a
James Pow <jbpow18@icloud.com>
2023-03-26 08:34:55 -0400

    fix: changed regex to deal with extra spaces and punctuation

--- php/numWords.php
+++ php/numWords.php
@@ -5,23 +5,29 @@
     if (empty($string)) 
       return 0;
 
-    $words = explode(' ', preg_replace('/\s+/', ' ', $string));
+    $words = preg_split('/[^\w]+/', $string, -1, PREG_SPLIT_NO_EMPTY);
 
+
+    
     return count($words);
   }
 
-// // Test case where the input string has no words
+// Test case where the input string has no words
 try {
   $string = "";
   assert(numWords($string) == 0, "Test case 1 failed: Expected 0 words but got " . numWords($string) . ".");
+  echo "Test Case 1 passed";
 } catch (AssertionError $e) {
   echo $e->getMessage();
 }
+
+echo "<br>";
 
 // Test case where the input string has one word
 try {
   $string = "Hello";
   assert(numWords($string) == 1, "Test case 2 failed: Expected 1 word but got " . numWords($string) . ".");
+  echo "Test Case 2 passed";
 } catch (AssertionError $e) {
   echo $e->getMessage();
 }
@@ -32,6 +38,7 @@ echo "<br>";
 try {
   $string = "Hello world! This is a test.";
   assert(numWords($string) == 6, "Test case 3 failed: Expected 6 words but got " . numWords($string) . ".");
+  echo "Test Case 3 passed";
 } catch (AssertionError $e) {
   echo $e->getMessage();
 }
@@ -42,6 +49,7 @@ echo "<br>";
 try {
   $string = "   Hello    world!    ";
   assert(numWords($string) == 2, "Test case 4 failed: Expected 2 words but got " . numWords($string) . ".");
+  echo "Test Case 4 passed";
 } catch (AssertionError $e) {
   echo $e->getMessage();
 }
@@ -52,6 +60,7 @@ echo "<br>";
 try {
   $string = "Hello@world This^is*a:test";
   assert(numWords($string) == 6, "Test case 5 failed: Expected 6 words but got " . numWords($string) . ".");
+  echo "Test Case 5 passed";
 } catch (AssertionError $e) {
   echo $e->getMessage();
 }
